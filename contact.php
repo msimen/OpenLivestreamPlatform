9f0558296f8e74c6d71dcd16598596033e7f0027
Jorys-Paulin <paulinjorys5@gmail.com>
2014-09-26 17:03:16 +0200

    Starting main pages. Added links in head

--- contact.php
+++ contact.php
@@ -1,5 +1,6 @@
 <?php
-
+session_start();
+include("config.php");
 ?>
 
 <!DOCTYPE html>
@@ -7,6 +8,8 @@
     <head>
         <meta charset="utf-8" />
         <title></title>
+        <link rel="stylesheet" href="css/foundation.css" />
+        <script src="js/vendor/modernizr.js"></script>
     </head>
     <body>
         
--- streamers.php
+++ streamers.php
@@ -1,5 +1,6 @@
 <?php
-
+session_start();
+include("config.php");
 ?>
 
 <!DOCTYPE html>
