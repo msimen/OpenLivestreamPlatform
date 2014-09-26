f2dfcd5e2b27d5d099045b3b400d7197c1ed6e23
Jorys-Paulin <paulinjorys5@gmail.com>
2014-09-26 16:56:24 +0200

    Add base files

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+/* 2014 Open livestream platform - Jorys paulin
+This site is open-source : fell free to edit, add things to it.
+Github source-code : https://github.com/Jorys-Paulin/openlivestreamplatform/
+*/
+
+//MySql database connection
+
+//New messages checker
+
+?>
