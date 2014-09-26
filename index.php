7ca281fed76786f12e38d695061421db90579a41
Jorys-Paulin <paulinjorys5@gmail.com>
2014-09-26 17:39:48 +0200

    Added jQuery and minecraft avatar composer (Usign GD)

--- index.php
+++ index.php
@@ -12,6 +12,10 @@ include("config.php");
         <script src="js/vendor/modernizr.js"></script>
     </head>
     <body>
-        
+        <script src="js/vendor/jquery.js"></script>
+        <script src="js/foundation.min.js"></script>
+        <script>
+            $(document).foundation();
+        </script>
     </body>
 </html>
--- streamers.php
+++ streamers.php
@@ -7,9 +7,15 @@ include("config.php");
 <html lang="en">
     <head>
         <meta charset="utf-8" />
-        <title></title>
+        <title>Streamers</title>
+        <link rel="stylesheet" href="css/foundation.css" />
+        <script src="js/vendor/modernizr.js"></script>
     </head>
     <body>
-        
+        <script src="js/vendor/jquery.js"></script>
+        <script src="js/foundation.min.js"></script>
+        <script>
+            $(document).foundation();
+        </script>
     </body>
 </html>
