fd9206e4f535a516d9629f6a99e22133e4ce46cb
Jorys-Paulin <paulinjorys5@gmail.com>
2014-09-26 17:22:29 +0200

    Started Easy games integration

--- config.php
+++ config.php
@@ -14,4 +14,21 @@ $global_platform['name']="Open livestream platform";
 $global_platform['version']="1.0";
 //Description of your livestream platform
 $global_platform['description']="Open livestream platform is platform developped with PHP/MySql to create, manage and edit livestreams";
+//Lang file
+$global_platform['lang_file']="lang/en_EN.php";
+
+//Easy games integration
+
+//Minecraft
+$global_games['minecraft_enabled']=FALSE;
+$global_games['minecraft_server']="Adress of the public server of your platform";
+//Dofus
+$global_games['dofus_enabled']=FALSE;
+$global_games['dofus_server']="Adress of the public server of your platform";
+//League of legends
+$global_games['lol_enabled']=FALSE;
+$global_games['lol_server']="Adress of the public server of your platform";
+//Watchdogs
+$global_games['wd_enabled']=FALSE;
+$global_games['wd_server']="Adress of the public server of your platform";
 ?>
