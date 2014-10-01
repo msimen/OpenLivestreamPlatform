064af54310baa6fd0bd4b38d3c08ebeb5e84ccf7
Jorys-Paulin <paulinjorys5@gmail.com>
2014-10-01 16:39:26 +0200

    Added lives modals, alerts, header, livestreams list

--- index.php
+++ index.php
@@ -72,8 +72,58 @@ include("config.php");
                 </ul>
             </aside>
             <!-- Content -->
-            <h1 id="header-welcome">Welcome to <?php echo($global_platform['name']);?> <span class="label [radius round]"><?php echo($global_platform['version']);?></span></h1>
-            <p id="header-description"><?php echo($global_platform['description']);?></p>
+            <div class="row">
+              <div class="small-11 small-centered columns">
+                <br/>
+                <div class="alerts">
+                    <div data-alert class="alert-box">
+                      <h4>You are connected</h4>
+                      <p>It's a little ostentatious, but useful for important content.</p>
+                    <a href="#" class="close">&times;</a>
+                    </div>
+                    <div data-alert class="alert-box secondary">
+                      <h5>This site uses HTML5 and CSS3</h5>
+                      <p>If your browser don't support them, please update.</p>
+                      <a href="#" class="close">&times;</a>
+                    </div>
+                    <!--<div class="panel callout radius">
+                      <h4>You are connected</h4>
+                      <p>It's a little ostentatious, but useful for important content.</p>
+                    </div>-->
+                    <!--<div class="panel">
+                      <h5>This site uses HTML5 and CSS3</h5>
+                      <p>If your browser don't support them, please update.</p>
+                    </div>-->
+                </div>
+                <header>
+                    <h1 id="header-welcome">Welcome to <?php echo($global_platform['name']);?> <span class="label [radius round]"><?php echo($global_platform['version']);?></span></h1>
+                    <p id="header-description"><?php echo($global_platform['description']);?></p>
+                </header>
+                <div class="lives modals">
+                    <div id="livemodal1" class="reveal-modal" data-reveal>
+                      <h2>Name of your livestream</h2>
+                      <p class="lead">By <b>Yourusername</b></p>
+                      <p>I'm a cool paragraph that lives inside of an even cooler modal. Wins!</p>
+                      <a class="close-reveal-modal">&#215;</a>
+                    </div>
+                </div>
+                <div class="lives list">
+                    <div class="row" data-equalizer>
+                      <div id="live_1" class="large-6 columns panel" data-equalizer-watch>
+                        <h2>Name of your livestream</h2>
+                        <p class="lead">By <b>Your username</b></p>
+                        <a href="#" data-reveal-id="livemodal1">More about this live</a>
+                      </div>
+                      <div id="live_2" class="large-6 columns panel" data-equalizer-watch>
+                        <h2>Name of your livestream</h2>
+                        <p class="lead">By <b>Your username</b></p>
+                        <a href="#" data-reveal-id="livemodal2">More about this live</a>
+                      </div>
+                    </div>
+                    
+                </div>
+              </div>
+            </div>
             <!-- Close the off-canevas menu -->
             <a class="exit-off-canvas"></a>
           </div>
