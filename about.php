dddeff3a95da56a79206780228bb80222d41c2da
Jorys-Paulin <paulinjorys5@gmail.com>
2015-01-30 16:12:05 +0100

    Redesigned about.php page

--- about.php
+++ about.php
@@ -151,8 +151,8 @@ include("config.php");
                 <section class="navbar">
                     <div class="container">
                         <div class="row">
-                            <div class="small-12 medium-4 columns title"><h4>Open livestream platform</h4></div>
-                            <div class="small-12 medium-8 columns text">&copy; 2015 Open livestream platform - <a href="index.php">Index page</a> - <a href="contact.php">Contact</a> - <a href="livestreams.php">Livestreams</a></div>
+                            <div class=" medium-4 large-4 columns title"><h4>Open livestream platform</h4></div>
+                            <div class=" medium-8 large-8 columns text">&copy; 2015 Open livestream platform - <a href="index.php">Index page</a> - <a href="contact.php">Contact</a> - <a href="livestreams.php">Livestreams</a></div>
                         </div>
                     </div>             
                 </section>
